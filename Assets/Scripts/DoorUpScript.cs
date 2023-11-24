eef4811b724a96f30f66c08bd55fea97206a2768
tarenstrannik <tarenstrannik@gmail.com>
2023-11-24 15:53:35 +0200

    doors and plates

--- Assets/Scripts/DoorUpScript.cs
+++ Assets/Scripts/DoorUpScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class DoorUpScript : MonoBehaviour
@@ -11,101 +12,168 @@ public class DoorUpScript : MonoBehaviour
 
 
     [SerializeField] private Transform[] m_startTransforms;
-    [SerializeField] private Transform[]  m_endTransforms;
+    [SerializeField] private Transform[] m_endTransforms;
 
 
     [SerializeField] GameObject m_pruty;
     [SerializeField] GameObject m_transparentDoor;
 
-    
 
-    private Coroutine m_MoveDoorCoroutine=null;
+
+    private Coroutine m_MoveDoorCoroutine = null;
+    private Coroutine m_MoveDoorSecondStageCoroutine = null;
     private float m_curTime;
+    private float m_curPercent;
 
     [SerializeField] private float[] m_stageMoveTime = { 3f, 3f };
     [SerializeField] float m_pauseTime = 3f;
+    [SerializeField] float m_MoveDownTimeSecondStage = 1f;
+    public float MoveDownTimeSecondStage { get; }
 
     private float m_startPitch;
 
     private int m_curStage = 0;
+    private int m_maxStage = 1;
     private void Awake()
     {
         m_doorAudioSource = GetComponent<AudioSource>();
         m_pruty.transform.position = m_startTransforms[0].position;
         m_transparentDoor.transform.position = m_startTransforms[0].position;
         m_startPitch = m_doorAudioSource.pitch;
+        m_maxStage = m_stageMoveTime.Length - 1;
     }
 
 
 
     public void MoveDoor(float startTimePercent, float endTimePercent, float allTime)
     {
-        if (m_curStage == 0)
+
+
+            m_doorAudioSource.pitch = m_startPitch * allTime / m_stageMoveTime[1];
+        GameObject[] objectsToMove;
+            if (m_curStage==0)
         {
-           
-            m_doorAudioSource.pitch = m_startPitch*allTime / m_stageMoveTime[1];
-            GameObject[] objectsToMove = { m_pruty };
+            GameObject[] obj = { m_pruty };
+            objectsToMove = obj;
+        }
+        else
+        {
+            GameObject[] obj = { m_pruty, m_transparentDoor };
+            objectsToMove = obj;
+        }
+        
             if (m_MoveDoorCoroutine != null)
             {
-                StopMovingDoorCoroutine(m_MoveDoorCoroutine);
+                StopMovingDoorCoroutine();
             }
             m_MoveDoorCoroutine = StartCoroutine(IMoveDoor(objectsToMove, startTimePercent, endTimePercent, allTime));
-        }
+
     }
-    private void StopMovingDoorCoroutine(Coroutine cor)
+    public void MoveDoorSecondStageDown()
     {
-        if(cor!=null)
+        m_doorAudioSource.pitch = m_startPitch * m_MoveDownTimeSecondStage / m_stageMoveTime[1];
+
+            GameObject[] obj = { m_pruty, m_transparentDoor };
+        GameObject[] objectsToMove = obj;
+
+
+        if (m_MoveDoorCoroutine != null)
         {
-            StopCoroutine(cor);
+            StopMovingDoorCoroutine();
+        }
+        m_MoveDoorCoroutine = StartCoroutine(IMoveDoor(objectsToMove, m_curPercent, 0, m_MoveDownTimeSecondStage*(1- m_curPercent)));
+
+    }
+    public void StopMovingDoorCoroutine()
+    {
+        if (m_MoveDoorCoroutine != null)
+        {
+            StopCoroutine(m_MoveDoorCoroutine);
             m_doorAudioSource.Pause();
+            m_MoveDoorCoroutine = null;
         }
     }
     private IEnumerator IMoveDoor(GameObject[] objectsToMove, float startTimePercent, float endTimePercent, float allTime)
     {
-        
+
 
         Vector3 startPosition = m_startTransforms[m_curStage].position;
         Vector3 endPosition = m_endTransforms[m_curStage].position;
 
 
-        int directionCoef= endTimePercent> startTimePercent ? 1 : -1;
-        
-        m_curTime = startTimePercent * allTime;
+        int directionCoef = endTimePercent > startTimePercent ? 1 : -1;
 
+        m_curTime = startTimePercent * allTime;
+        m_curPercent = startTimePercent;
         m_doorAudioSource.Play();
 
-        while (directionCoef*m_curTime <= directionCoef*endTimePercent * allTime)
+        while (directionCoef * m_curTime <= directionCoef * endTimePercent * allTime)
         {
-            foreach(var obj in objectsToMove)
+            foreach (var obj in objectsToMove)
             {
-                
-                obj.transform.position = Vector3.Lerp(startPosition, endPosition, m_curTime/ allTime);
+
+                obj.transform.position = Vector3.Lerp(startPosition, endPosition, m_curTime / allTime);
             }
-            m_curTime += directionCoef*Time.deltaTime;
-            
+            m_curTime += directionCoef * Time.deltaTime;
+            m_curPercent = m_curTime / allTime;
             yield return null;
         }
+        m_curPercent = m_curTime / allTime;
         m_doorAudioSource.Pause();
-        if (m_curTime/ allTime<=0 || m_curTime / allTime >=1)
+        if (m_curTime / allTime <= 0 || m_curTime / allTime >= 1)
         {
             m_doorSlamAudioSource.PlayOneShot(m_movingEndedSound);
         }
-              
+
         m_MoveDoorCoroutine = null;
 
-        if(m_curStage==0 && m_curTime / allTime >= 1)
+        if (m_curTime / allTime >= 1 && m_curStage==0)
         {
             m_curStage = 1;
-            yield return new WaitForSeconds(m_pauseTime);
-            GameObject[] obj = { m_pruty, m_transparentDoor };
-            m_doorAudioSource.pitch = m_startPitch;
-            m_MoveDoorCoroutine =StartCoroutine(IMoveDoor(obj, 0, 1, m_stageMoveTime[m_curStage]));
+            m_curPercent = 0;
+        }
+        else if (m_curTime / allTime<=0 && m_curStage ==1)
+        {
+            m_curStage = 0;
+            m_curPercent = 1;
         }
     }
-
+    /*--------------------for handle--------------*/
     public void MoveDoorFullFirstStage()
     {
         m_doorAudioSource.pitch = m_startPitch * m_stageMoveTime[0] / m_stageMoveTime[1];
-        MoveDoor(0,1, m_stageMoveTime[m_curStage]);
+        MoveDoor(0, 1, m_stageMoveTime[m_curStage]);
+
+        MoveDoorSecondStageUp();
     }
+    public void MoveDoorSecondStageUp()
+    {
+        if(m_MoveDoorSecondStageCoroutine!=null)
+        {
+            StopSecondStage();
+        }
+        m_MoveDoorSecondStageCoroutine=StartCoroutine(WaitAndInitSecondStage());
+    }
+
+    
+    public void StopSecondStage()
+    {
+        StopCoroutine(m_MoveDoorSecondStageCoroutine);
+        m_MoveDoorSecondStageCoroutine = null;
+    }
+    private IEnumerator WaitAndInitSecondStage()
+    {
+        while (m_curStage == 0)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(m_pauseTime);
+        GameObject[] obj = { m_pruty, m_transparentDoor };
+        m_doorAudioSource.pitch = m_startPitch;
+        
+        m_MoveDoorCoroutine = StartCoroutine(IMoveDoor(obj, m_curPercent, 1, m_stageMoveTime[m_curStage]*(1- m_curPercent)));
+    }
+
+     
 }
--- Assets/Scripts/PressingPlateScript.cs
+++ Assets/Scripts/PressingPlateScript.cs
@@ -4,15 +4,141 @@ using UnityEngine;
 
 public class PressingPlateScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private AudioSource m_plateAudioSource;
+    [SerializeField] private AudioSource m_plateClickAudioSource;
+    [SerializeField] private AudioClip m_movingEndedClick;
+
+
+    [SerializeField] private Transform[] m_startTransforms;
+    [SerializeField] private Transform[] m_endTransforms;
+
+
+    [SerializeField] GameObject m_plate;
+
+
+    private Coroutine m_MoveplateCoroutine = null;
+    private float m_curTime;
+
+    [SerializeField] private float[] m_stageMoveTime = { 3f};
+    private float m_doorFallingTimeout = 1f;
+    private float m_curWaitingTimeout = 0f;
+    [SerializeField] private GameObject m_LinkedDoor;
+    
+
+    [SerializeField] private float[] m_maxMass = {60f };
+    private float m_curMass = 0f;
+
+    private List<GameObject> m_currentColliders = new List<GameObject>();
+
+    private void Awake()
     {
-        
+
+        m_plate.transform.position = m_startTransforms[0].position;
+        m_doorFallingTimeout = m_LinkedDoor.GetComponent<DoorUpScript>().MoveDownTimeSecondStage;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void Moveplate(float startTimePercent, float endTimePercent, float allTime)
     {
-        
+
+            
+            GameObject[] objectsToMove = { m_plate };
+            if (m_MoveplateCoroutine != null)
+            {
+                StopMovingplateCoroutine(m_MoveplateCoroutine);
+            }
+            m_MoveplateCoroutine = StartCoroutine(IMoveplate(objectsToMove, startTimePercent, endTimePercent, allTime));
+        SendMoveCommandToDoorWithDelay(m_curWaitingTimeout,startTimePercent, endTimePercent, allTime);
+           
+    }
+
+    private IEnumerator SendMoveCommandToDoorWithDelay(float delay, float startTimePercent, float endTimePercent, float allTime)
+    {
+        yield return new WaitForSeconds(delay);
+        m_LinkedDoor.GetComponent<DoorUpScript>().MoveDoor(startTimePercent, endTimePercent, allTime);
+    }
+
+    private IEnumerator DecreasingDelay()
+    {
+        m_curWaitingTimeout = m_doorFallingTimeout;
+        while (m_curWaitingTimeout>=0)
+        {
+            m_curWaitingTimeout -= Time.deltaTime;
+            yield return null;
+        }
+    }
+    private void StopMovingplateCoroutine(Coroutine cor)
+    {
+        if (cor != null)
+        {
+            StopCoroutine(cor);
+            m_plateAudioSource.Pause();
+            m_LinkedDoor.GetComponent<DoorUpScript>().StopMovingDoorCoroutine();
+        }
+    }
+    private IEnumerator IMoveplate(GameObject[] objectsToMove, float startTimePercent, float endTimePercent, float allTime)
+    {
+
+
+        Vector3 startPosition = m_startTransforms[0].position;
+        Vector3 endPosition = m_endTransforms[0].position;
+
+
+        int directionCoef = endTimePercent > startTimePercent ? 1 : -1;
+
+        m_curTime = startTimePercent * allTime;
+
+        m_plateAudioSource.Play();
+
+        while (directionCoef * m_curTime <= directionCoef * endTimePercent * allTime)
+        {
+            foreach (var obj in objectsToMove)
+            {
+
+                obj.transform.position = Vector3.Lerp(startPosition, endPosition, m_curTime / allTime);
+            }
+            m_curTime += directionCoef * Time.deltaTime;
+
+            yield return null;
+        }
+        m_plateAudioSource.Pause();
+        if (m_curTime / allTime >= 1)
+        {
+            m_plateClickAudioSource.PlayOneShot(m_movingEndedClick);
+        }
+
+        m_MoveplateCoroutine = null;
+
+        if ( m_curTime / allTime >= 1)
+        {
+           
+            
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        var colRb = collision.gameObject.GetComponent<Rigidbody>();
+        if (!m_currentColliders.Contains(collision.gameObject) && colRb != null)
+        {
+            m_currentColliders.Add(collision.gameObject);
+            var prevMass = m_curMass;
+            m_curMass += colRb.mass;
+
+            Moveplate(prevMass / m_maxMass[0], m_curMass / m_maxMass[0], m_stageMoveTime[0]);
+            
+
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        m_currentColliders.Remove(collision.gameObject);
+        var prevMass = m_curMass;
+        m_curMass -= collision.gameObject.GetComponent<Rigidbody>().mass;
+        Moveplate(prevMass / m_maxMass[0], m_curMass / m_maxMass[0], m_stageMoveTime[0]);
     }
 }
